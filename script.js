bacee1518af21eb44e4aa53460096e845e2a326f
manoharsena <manoharsena@gmail.com>
2023-12-20 20:36:49 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+// inp.cursor()
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+  console.log(userInput);
+
+ 
+  //end-here
+});
+
